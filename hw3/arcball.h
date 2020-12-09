038b0f358d4b834738b1ed60c0a69186530205c9
Xiaoqi-Long-Caltech <57379702+Xiaoqi-Long-Caltech@users.noreply.github.com>
2020-12-09 23:56:05 +0800

    Add files via upload

---
+++ hw3/arcball.h
@@ -0,0 +1,119 @@
+
+using namespace std;
+
+/*Struct for quaternions. Named them with double n so as to avoid confusion with class in Eigen.
+*/
+struct Quaternionn {
+   float s, x, y, z;
+   Quaternionn() : s(1), x(0), y(0), z(0) {} //creates a quaternion identity
+   Quaternionn(float s_, float x_, float y_, float z_)
+      : s(s_), x(x_), y(y_), z(z_) {}
+      
+   //define multiplication for quaternions
+   void operator*=(const Quaternionn& q) {
+      float new_s = s * q.s - this->vdot(q);
+      float new_x = s * q.x + q.s * x + (y * q.z - z * q.y);
+      float new_y = s * q.y + q.s * y + (z * q.x - x * q.z);
+      float new_z = s * q.z + q.s * z + (x * q.y - y * q.x);
+      s = new_s;
+      x = new_x;
+      y = new_y;
+      z = new_z;
+   }
+   
+   Quaternionn operator*(const Quaternionn& q) {
+      Quaternionn res = *this;
+      res *= q;
+      return res;
+   }
+   
+   float vdot(const Quaternionn& q) const {
+      return x * q.x + y * q.y + z * q.z;
+   }
+   
+   /** Get rotation matrix from quaternion. For some reason, I had to alter some signs in this matrix from the lecture notes to get the object rotate in an intuitive manner. **/
+   
+   array<float, 16> GetRotationMatrix() {
+      array<float, 16> res;
+      // col 1
+      res[0] =  1 - 2 * y * y - 2 * z * z;
+      res[1] =  2 * (x * y + z * s);
+      res[2] =  2 * (x * z - y * s);
+      res[3] =  0;
+      
+      // col 2
+      res[4] = 2 * (x * y - z * s);
+      res[5] = 1 - 2 * x * x - 2 * z * z;
+      res[6] = 2 * (y * z + x * s);
+      res[7] = 0;
+      
+      // col 3
+      res[8] =  2 * (x * z + y * s);
+      res[9] = 2 * (y * z - x * s);
+      res[10] = 1 - 2 * x * x - 2 * y * y;
+      res[11] = 0;
+      
+      // col 4
+      res[12] = 0;
+      res[13] = 0;
+      res[14] = 0;
+      res[15] = 1;
+       return res;
+   }
+   
+};
+
+/* convert from screen coordinates to NDC coordinates: simply invert NDC_to_screen
+*/
+float screen_to_NDC(int coord, int res) {
+    float ndc = (2.0 * coord) / res - 1.0;
+    return ndc;
+}
+
+/* unit vector of rotation axis */
+Vector3f get_rot_axis(int x0, int y0, int x1, int y1, int xres, int yres) {
+    float ndc_x0 = screen_to_NDC(x0, xres);
+    float ndc_y0 = -screen_to_NDC(y0, yres);
+    float ndc_x1 = screen_to_NDC(x1, xres);
+    float ndc_y1 = -screen_to_NDC(y1, yres);
+    float ndc_z0 = 1 - pow(ndc_x0, 2) - pow(ndc_y0, 2);
+    ndc_z0 = (ndc_z0 > 0) ? sqrt(ndc_z0) : 0;
+    float ndc_z1 = 1 - pow(ndc_x1, 2) - pow(ndc_y1, 2);
+    ndc_z1 = (ndc_z1 > 0) ? sqrt(ndc_z1) : 0;
+    //unit rotation axis
+    Vector3f ndc_v0(ndc_x0, ndc_y0, ndc_z0);
+    Vector3f ndc_v1(ndc_x1, ndc_y1, ndc_z1);
+    Vector3f uni_rot_axis = ndc_v0.cross(ndc_v1);
+    if (uni_rot_axis.norm() != 0)
+    	uni_rot_axis.normalize();
+    return uni_rot_axis;
+}
+
+/* rotation angle */
+float get_rot_angle(int x0, int y0, int x1, int y1, int xres, int yres) {
+    float ndc_x0 = screen_to_NDC(x0, xres);
+    float ndc_y0 = -screen_to_NDC(y0, yres);
+    float ndc_x1 = screen_to_NDC(x1, xres);
+    float ndc_y1 = -screen_to_NDC(y1, yres);
+    float ndc_z0 = 1 - pow(ndc_x0, 2) - pow(ndc_y0, 2);
+    ndc_z0 = (ndc_z0 > 0) ? sqrt(ndc_z0) : 0;
+    float ndc_z1 = 1 - pow(ndc_x1, 2) - pow(ndc_y1, 2);
+    ndc_z1 = (ndc_z1 > 0) ? sqrt(ndc_z1) : 0;
+    //rotation angle
+    Vector3f ndc_v0(ndc_x0, ndc_y0, ndc_z0);
+    Vector3f ndc_v1(ndc_x1, ndc_y1, ndc_z1);
+    float prod = ndc_v0.dot(ndc_v1);
+    prod /= (ndc_v0.norm()) * (ndc_v1.norm());
+    return acos(min((float)1.0, prod));
+}
+
+/* get rotation quaternion given coordinates */
+Quaternionn get_rot_quaternion(int x0, int y0, int x1, int y1, int xres, int yres) {
+    float rot_angle = get_rot_angle(x0, y0, x1, y1, xres, yres);
+    Vector3f rot_axis = get_rot_axis(x0, y0, x1, y1, xres, yres);
+    float real = cos(rot_angle / 2);
+    float im_scalar = sin(rot_angle / 2);
+    Vector3f image = rot_axis * im_scalar;
+    Quaternionn q(real, image(0, 0), image(1, 0), image(2, 0));
+    return q;
+}
---
+++ hw3/parse_scene.h
@@ -0,0 +1,201 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <cmath>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <unordered_map>
+#include "structures.h"
+
+
+using namespace std;
+const string DATA_DIR = "data/";
+
+/**Part 1: Parse the scene description file and put the data in a 
+Scene data structure **/
+
+/**parse the object **/
+
+Object parse_object(string filename) {
+        string filename_ = DATA_DIR;
+	filename_.append(filename);
+        vector<Triple> vertex_buffer;
+        vector<Triple> normal_buffer;
+        vector<Triple> vertex_sets;
+        vector<Triple> normal_sets;
+        vertex_sets.push_back(Triple()); // 1-index vertices
+        normal_sets.push_back(Triple()); // 1-index normals
+        ifstream infile(filename_);
+        string t;
+        while (infile >> t ) {
+            if (t.compare("v") == 0) { //storing a vertex in our data structure
+                float x, y, z;
+                infile >> x >> y >> z;
+                Triple v(x, y, z);
+                vertex_sets.push_back(v); 
+            }
+            else if (t.compare("vn") == 0) {//storing a surface normal in our data structure
+                float x, y, z;
+                infile >> x >> y >> z;
+                Triple vn(x, y, z);
+                normal_sets.push_back(vn);
+            } else {
+            	 string a, b, c;
+            	 infile >> a >> b >> c;
+            	 string v1s = a.substr(0, a.find("//"));
+            	 string vn1s = a.substr(a.find("//") + 2);
+            	 int v1 = stoi(v1s);
+            	 int vn1 = stoi(vn1s);
+            	 string v2s = b.substr(0, b.find("//"));
+            	 string vn2s = b.substr(b.find("//") + 2);
+            	 int v2 = stoi(v2s);
+            	 int vn2 = stoi(vn2s);
+            	 string v3s = c.substr(0, c.find("//"));
+            	 string vn3s = c.substr(c.find("//") + 2);
+            	 int v3 = stoi(v3s);
+            	 int vn3 = stoi(vn3s);
+            	 //create the large arrays
+            	 vertex_buffer.push_back(vertex_sets[v1]);
+            	 vertex_buffer.push_back(vertex_sets[v2]);
+            	 vertex_buffer.push_back(vertex_sets[v3]);
+            	 normal_buffer.push_back(normal_sets[vn1]);
+            	 normal_buffer.push_back(normal_sets[vn2]);
+            	 normal_buffer.push_back(normal_sets[vn3]); 
+            }
+        }
+        Object o(vertex_buffer, normal_buffer);
+        return o;
+}
+
+vector<Object> parse_objects(ifstream &infile) {
+    unordered_map<string, Object> labels;
+    vector<Object> object_copies;
+    string line;
+    getline(infile, line);
+    
+    /* the labels and corresponding filenames come up before an empty line
+       so fill in the map before encountering empty line */
+    while (!line.empty()) {
+        istringstream iss(line);
+        string label, file;
+        iss >> label >> file;
+        labels[label] = parse_object(file);
+        getline(infile, line);
+    }
+    
+    /* read the rest of the file, i.e. labels of objects and transformations */
+    while (getline(infile, line)) {
+        //first line is a label for an object
+        string label = line;
+        Object obj = labels[label];
+        getline(infile, line);
+        Matrix4f m;
+        //next few nonempty lines are transformation matrices for given object or material parameters. Also material parameters.
+        while (!line.empty()) {
+            string t;
+            istringstream iss(line);
+            iss >> t;
+            if (t.compare("t") == 0 || t.compare("s") == 0) {
+                float x, y, z;
+                iss >> x >> y >> z;
+                Transforms transform(t, x, y, z);
+                obj.transform_sets.push_back(transform);
+            }
+            else if (t.compare("r") == 0) {
+                float x, y, z, angle;
+                iss >> x >> y >> z >> angle;
+                Transforms transform(t, x, y, z, angle);
+                obj.transform_sets.push_back(transform);
+            }
+            else if (t.compare("ambient") == 0) {
+                float r, g, b;
+                iss >> r >> g >> b;
+                obj.SetAmbient(r, g, b);
+	     }
+	    else if (t.compare("diffuse") == 0) {
+	        float r, g, b;
+                iss >> r >> g >> b;
+                obj.SetDiffuse(r, g, b);
+	    }
+	    else if (t.compare("specular") == 0) {
+	        float r, g, b;
+                iss >> r >> g >> b;
+                obj.SetSpecular(r, g, b);
+	    }
+	    else if (t.compare("shininess") == 0) {
+	        float shine;
+                iss >> shine;
+                obj.shininess = shine;
+	    }
+            if (infile.eof())
+                break;
+            getline(infile, line);
+        }
+    object_copies.push_back(obj);
+    }
+    return object_copies;
+}
+
+Scene parse_camera(ifstream &infile) {
+	Scene scene;
+	string line, t;
+	float x, y, z, angle, p, r, g, b, atten;
+	char van;
+	//read camera parameters
+	while (getline(infile, line)) {
+	if (line.compare("objects:") == 0) {
+            scene.objects = parse_objects(infile); //call parse_objects when reaching objects
+            break;
+        } else {
+		istringstream iss(line);
+		iss >> t;
+		if (t.compare("camera:") == 0) {
+			continue;
+		}
+		else if (t.compare("position") == 0) {
+			iss >> x >> y >> z;
+			Triple position_camera(x, y, z);
+			scene.position_camera = position_camera;
+		}
+		else if (t.compare("orientation") == 0) {
+			iss >> x >> y >> z >> angle;
+			Triple orientation_camera(x, y, z);
+			scene.orientation_camera = orientation_camera;
+			scene.orientation_angle_cam = angle;
+		}
+		else if (t.compare("near") == 0) {
+			iss >> p;
+			scene.near = p;
+		}
+		else if (t.compare("far") == 0) {
+			iss >> p;
+			scene.far = p;
+		}
+		else if (t.compare("left") == 0) {
+			iss >> p;
+			scene.left = p;
+		}
+		else if (t.compare("right") == 0) {
+			iss >> p;
+			scene.right = p;
+		}
+		else if (t.compare("top") == 0) {
+			iss >> p;
+			scene.top = p;
+		}
+		else if (t.compare("bottom") == 0) {
+			iss >> p;
+			scene.bottom = p;
+		}
+		else if (t.compare("light") == 0) {
+			iss >> x >> y >> z >> van >> r >> g >> b >> van >> atten;
+			Point_Light l(x, y, z, 1, r, g, b, atten);
+			scene.lights.push_back(l);
+		}
+	 }
+	}
+	return scene;
+}
+
+#endif
